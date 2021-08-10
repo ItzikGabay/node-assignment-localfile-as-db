467f5817eee6fc204b9d9ccccb902959753eeabb
Itzik <itzikdevio@gmail.com>
2021-08-10 14:39:30 +0300

    stable version before changes

--- app.js
+++ app.js
@@ -1,36 +1,26 @@
-/**
+/*****************
  * * app.js - main app file
- **/
-'use strict';
+ *****************/
+"use strict";
 
-
-/**
+/*****************
  * Imports
- **/
-const express = require('express');
+ *****************/
+const express = require("express");
 const app = express();
-const router = require('./routes/index.js')
+const router = require("./routes/index.js");
 
-
-/**
+/*****************
  * App Settings
- **/
-app.use(express.json())
+ *****************/
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// Router
 app.use("/", router);
 
-
-/**
- * Routes
- **/
-app.get('/1', (req, res) => {
-    res.status(200).send('hello')
-})
-
-/**
+/*****************
  * App listening..
- **/
+ *****************/
 app.listen(3000, (req, res) => {
-    console.log('Started');
-})
+  console.log("Started");
+});
