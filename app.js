dc376e4d276587449f0403348497d7826be2b589
Itzik <itzikdevio@gmail.com>
2021-08-15 15:07:27 +0300

    stable version with async + start of validation

--- app.js
+++ app.js
@@ -18,6 +18,29 @@ app.use(express.urlencoded({ extended: true }));
 // Router
 app.use("/", router);
 
+_useErrorMiddlewares();
+
+function _useErrorMiddlewares() {
+  app.use((err, req, res, next) => {
+    // request aborted handler
+    if (err.status === 400 && err.code === "ECONNABORTED") {
+      return res.status(err.status).json({
+        error: err,
+      });
+    }
+
+    if (err.name !== "JsonSchemaValidation") {
+      return next(err);
+    }
+
+    res.status(400).json({
+      statusText: "Bad Request",
+      jsonSchemaValidation: true,
+      errors: err.validations, // All the validation information
+    });
+  });
+}
+
 /*****************
  * App listening..
  *****************/
--- routes/items/index.js
+++ routes/items/index.js
@@ -6,9 +6,26 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 
-
 // items Functions file ->
 const itemsLogic = require("./logic");
+
+// Schema 
+const validate = require("express-jsonschema").validate;
+const itemsSchema = {
+  query: {
+    type: "object",
+    additionalProperties: false,
+    properties: {
+      priceIsGreater: {
+        type: ["number", "string"],
+        format: "numeric",
+        minimum: 0,
+        maximum: 1,
+        required: true,
+      },
+    },
+  },
+};
 
 
 // items/item ROUTE ->
@@ -17,10 +34,10 @@ router.use("/:itemID", itemRoute);
 
 
 // items/ - GET ->
-router.get('/', async (req, res) => {
-  const result = await itemsLogic.getItems(req.query)
+router.get("/", validate(itemsSchema), async (req, res) => {
+  const result = await itemsLogic.getItems(req.query);
   res.status(200).json({ result });
-})
+});
 
 
 // items/ - POST ->
